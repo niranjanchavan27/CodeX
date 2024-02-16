458f9dae8e4a874625d500e3c67c9392df19aa28
niranjan <niranjanchavan2711@gmail.com>
2024-02-17 00:21:32 +0530

    sql file added

---
+++ Abbrevation.sql
@@ -0,0 +1,13 @@
+-- abbrevations :
+
+-- %a : used for weekday name (sun , mon)
+
+-- %w : used for weekday (sunday , monday)
+
+-- %b : used for month name (jan , feb)
+
+-- %M : used for month name (january april)
+
+-- %D : used for (1st , 2nd)
+
+-- %Y : used for year
---
+++ DateTimeFunctions.sql
@@ -0,0 +1,22 @@
+-- DateTime Functions:
+
+-- select curdate();
+
+-- select curtime();
+
+-- select now();
+
+-- create table stud1(
+--     name varchar(255),
+--     birthdate date,
+--     birthtime time,
+--     birthdt datetime
+-- );
+
+-- insert into stud1(name,birthdate,birthtime,birthdt)
+--             values("rohan","2004-08-01","12:01:00","2004-08-01 12:01:00");
+
+-- create table captions(
+-- text varchar(255),
+-- createAT TIMESTAMP 
+-- default CURRENT_TIMESTAMP);
